ca56ddd178c408f74969d0c6f5831446439f8a91
rizwanzaman65 <rizwan2203e@aptechgdn.net>
2024-12-13 11:01:37 +0500

    Zeeshan

---
+++ js/4.js
@@ -0,0 +1,8 @@
+// function changeTab(tabId) {
+//     var tab = document.getElementById(tabId);
+//     var tabPane = new bootstrap.Tab(tab);
+//     var targetPaneId = tab.getAttribute('data-bs-target');
+//     var targetPane = document.querySelector(targetPaneId);
+//     var paneInstance = new bootstrap.Tab(targetPane);
+//     paneInstance.show();
+// }
